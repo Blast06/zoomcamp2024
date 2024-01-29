6f2521878da0326ea26c36d284a054ce779874a6
Julio Delgado Tapia <lacalculadora06@hotmail.com>
2024-01-29 21:25:25 +0000

    init 1.1

---
+++ 2_docker_sql/pipeline.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pandas
+
+# some fancy stuff with pandas jejeje like loading csv file
+print(sys.argv)
+
+day = sys.argv[1]
+
+print('Job finished succesfully for day = f{day} ')
